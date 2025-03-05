d12081ef5867036007cc0d0a06a9e1062315f9d4
Vijay2K1 <vas072001@gmail.com>
2025-03-05 17:40:55 +0530

    Initial commit

---
+++ GlAssignment_D4/client/src/themes.js
@@ -0,0 +1,15 @@
+// src/themes.js
+export const lightTheme = {
+    background: "#ffffff",
+    color: "#000000",
+    buttonBackground: "#4CAF50",
+    buttonText: "#fff",
+  };
+  
+  export const darkTheme = {
+    background: "#333333",
+    color: "#000000",
+    buttonBackground: "#008CBA",
+    buttonText: "#fff",
+  };
+  
